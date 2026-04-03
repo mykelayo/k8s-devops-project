c3da86c15b1b302e6639eb09ee9cf13da129b78a
Michael Bello <56143067+mykelayo@users.noreply.github.com>
2026-04-03 21:18:57 +0000

    fix bugs

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -151,7 +151,7 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "${var.cluster_name}-managed-node-group"
   node_role_arn   = aws_iam_role.eks_node_group.arn
   subnet_ids      = var.private_subnet_ids
-  instance_types  = var.node_instance_types  # ← Moved here
+  instance_types  = var.node_instance_types 
 
   scaling_config {
     desired_size = var.desired_node_count
